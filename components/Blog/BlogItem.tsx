83e8fd6654d3259fc5136c443994b30e290e9d7c
imhotep <r.sipchenko@gmail.com>
2023-04-07 22:51:11 -0600

    fix image sizing for blog items

--- components/Blog/BlogItem.tsx
+++ components/Blog/BlogItem.tsx
@@ -37,9 +37,9 @@ export const BlogItem = ({ post }: Props) => {
       target={"_blank"}
       rel="noreferrer"
     >
-      <div className="group relative">
-        <img src={post.thumbnail} alt="image" width={500} height={500} />
-        <span className="absolute inset-0 block bg-gradient-to-b from-indigo-800 to-indigo-500 bg-cover bg-center bg-no-repeat opacity-10 transition-opacity group-hover:opacity-50"></span>
+      <div className="group relative h-72">
+        <img src={post.thumbnail} alt="image" className="h-72 w-full object-cover"/>
+        {/* <span className="absolute inset-0 block bg-gradient-to-b from-indigo-800 to-indigo-500 bg-cover bg-center bg-no-repeat opacity-10 transition-opacity group-hover:opacity-50"></span> */}
         <span className="absolute right-0 bottom-0 mr-4 mb-4 block rounded-full border-2 border-white px-6 py-2 text-center font-body text-sm font-bold uppercase text-white md:text-base">
           Read More
         </span>
