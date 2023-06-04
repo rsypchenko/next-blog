adc85aec181bf2d418052910088cd138ddafdc98
imhotep <r.sipchenko@gmail.com>
2023-06-04 10:40:53 -0600

    remove page transition animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { AnimatePresence } from "framer-motion";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AnimatePresence
-      mode="wait"
-      initial={false}
-      onExitComplete={() => window.scrollTo(0, 0)}
-    >
-      <Component {...pageProps} />
-    </AnimatePresence>
-  );
+  return <Component {...pageProps} />;
 }
