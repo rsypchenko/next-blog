ad011e4f56dd503a160c62eaaee025d85327d18a
imhotep <r.sipchenko@gmail.com>
2023-07-16 16:03:20 -0600

    add scroll spy hook for main menu

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -4,12 +4,22 @@ import { MenuItem } from "./MenuItem";
 import { DownloadButton } from "./DownloadButton";
 import Link from "next/link";
 
+import { useScrollspy } from "./useScrollSpy";
+import { useEffect } from "react";
+
 interface Props {
   secondary?: boolean;
 }
 
 export const Menu = ({ secondary }: Props) => {
-  const bg = secondary ? "inset-0 z-20 bg-gradient-to-r from-indigo-600 to-violet-500 opacity-100" : "bg-transparent";
+  const activeMenu = useScrollspy(["about", "skills", "blog"], 20);
+  const bg = secondary
+    ? "inset-0 z-20 bg-gradient-to-r from-indigo-600 to-violet-500 opacity-100"
+    : "bg-transparent";
+
+  useEffect(() => {
+    location.hash = `#${activeMenu}`;
+  }, [activeMenu]);
 
   return (
     <div className={`w-full z-50 top-0 py-3 px-5 sm:py-5 absolute ${bg} h-32`}>
@@ -20,9 +30,9 @@ export const Menu = ({ secondary }: Props) => {
 
         <div className="hidden sm:block">
           <ul className="flex items-center">
-            <MenuItem linkTo={"/#about"} text="About" />
-            <MenuItem linkTo={"/#skills"} text="Tech Skills" />
-            <MenuItem linkTo={"/blog"} text="blog" />
+            <MenuItem linkTo={"#about"} text="About" />
+            <MenuItem linkTo={"#skills"} text="Tech Skills" />
+            <MenuItem linkTo={"#blog"} text="blog" />
             {/* <MenuItem linkTo={"#contacts"} text="Contact" /> */}
           </ul>
         </div>
